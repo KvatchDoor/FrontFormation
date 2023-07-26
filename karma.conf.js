ab0b719acd1bd85dfcfa29e119cd0d632e198183
KvatchDoor <leftbehind31@gmail.com>
2023-07-26 22:21:37 +0200

    pass tests atrociously

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,14 @@
-browsers: ['ChromeHeadlessCI'],
-  customLaunchers: {
-  ChromeHeadlessCI: {
-    base: 'ChromeHeadless',
-      flags: ['--no-sandbox']
-  }
+// karma.conf.js
+process.env.CHROME_BIN = require('puppeteer').executablePath() // IMPORTANT!
+
+module.exports = function (config) {
+  config.set({
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    }
+  })
 }
